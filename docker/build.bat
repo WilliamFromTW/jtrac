d039331a7450ec86e05ca34c19a1c818907fc3a8
william <william.chen@hlmt.com.tw>
2026-09-12 09:16:20 +0800

    add latest

--- docker/build.bat
+++ docker/build.bat
@@ -3,6 +3,7 @@ setlocal
 cd /d "%~dp0"
 echo ==> Building JTrac Docker image from context: ..
 docker build -f Dockerfile -t jtrac:latest -t jtrac:2.3.3-2.1.0-beta ..
+docker build -f Dockerfile -t jtrac:latest -t jtrac:latest ..
 if %ERRORLEVEL% equ 0 (
     echo ==> Build complete! Images: jtrac:latest, jtrac:2.3.3-2.1.0-beta
 ) else (
--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,5 @@ cd "$SCRIPT_DIR"
 
 echo "==> Building JTrac Docker image from context: .."
 docker build -f Dockerfile -t jtrac:latest -t jtrac:2.3.3-2.1.0-beta ..
+docker build -f Dockerfile -t jtrac:latest -t jtrac:latest ..
 echo "==> Build complete! Images: jtrac:latest, jtrac:2.3.3-2.1.0-beta"
